a3045e21f5f3f448cc565bde74b18a3d3714dd79
Mario Cadenas <MarioCadenas@users.noreply.github.com>
2019-03-24 23:07:33 +0100

    events

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import { LitElement, html } from "lit-element";
 class MyElement extends LitElement {
   static get properties() {
     return {
-      feeling: String
+      foo: String
     };
+  }
+
+  shouldUpdate() {
+    console.log("rendered");
+    return true;
+  }
+
+  sayHi() {
+    console.log("Hi!");
   }
 
   render() {
     return html`
       <!-- template content -->
-      <p>A paragraph ${this.feeling}</p>
+      <p>A paragraph ${this.foo}</p>
+      <button @click="${this.sayHi}">Say hi!</button>
     `;
   }
 }
